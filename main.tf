06466b21c3c94933667798ab6446c720cb15a878
Tom Schiffmann <tom.schiffmann@it-accelerator.com>
2026-03-23 20:01:55 +0100

    add deployment

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
 
   backend "s3" {
-    bucket = "s3-tfstate-2026-03-09"
+    bucket = "2026-03-23-bs"
     key    = "app/terraform.tfstate" # Key könnt ihr selbst bestimmen, sollte nur über die deployments einheitlich sein
     region = "us-east-1"
   }
@@ -18,14 +18,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "demo" {
-  ami           = "ami-0b6c6ebed2801a5cb" # ubuntu
-  instance_type = "t2.micro"
+resource "aws_security_group" "ssh" {
+  name        = "allow-ssh"
+  description = "Allow SSH inbound traffic"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "demo" {
+  ami                    = "ami-0b6c6ebed2801a5cb" # ubuntu
+  instance_type          = "t2.micro"
+  key_name               = "my-key-pair"
+  vpc_security_group_ids = [aws_security_group.ssh.id]
   tags = {
     Name = "test-server-iac"
   }
 }
+
 
 output "instance_id" {
   value = aws_instance.demo.id
